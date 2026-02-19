151851cb235f660b4ec93d4d3f51ca9968e60796
Achmad Nazilul Fikri <achmad.nazilul@kelaspintar.id>
2026-02-19 10:56:39 +0700

    add echo command

--- app/main.go
+++ app/main.go
@@ -12,10 +12,15 @@ func main() {
 	for {
 		fmt.Print("$ ")
 		command, err := reader.ReadString('\n')
-		command = strings.TrimSpace(command)
+		sliceOfCommands := strings.Split(strings.TrimSpace(command), " ")
+		command = sliceOfCommands[0]
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
+		}
+		if command == "echo" {
+			fmt.Println(strings.Join(sliceOfCommands[1:], " "))
+			continue
 		}
 		if command == "exit" {
 			break
